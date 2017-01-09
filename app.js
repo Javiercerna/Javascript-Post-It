1dabea6006f68c808d55c859cb5f670f6b4bb972
Javiercerna <javier.cerna@pucp.pe>
2017-01-08 22:40:51 -0500

    Add Post It options and show them on hover

--- app.js
+++ app.js
@@ -4,7 +4,10 @@
   app.controller('BoardController',function() {
     this.post_its = [];
     this.addPostIt = function() {
-      this.post_its.push({'text': ''});
+      this.post_its.push({
+                 text : 'Change this text by clicking on the edit icon',
+        options_shown : false
+      });
     };
   });
 

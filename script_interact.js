7a0cc05c046a9d868462c9ab1966d37e705bdc80
Javiercerna <javier.cerna@pucp.pe>
2017-01-10 21:30:12 -0500

    Remove default cursor for Post It, restrict in div

--- script_interact.js
+++ script_interact.js
@@ -1,6 +1,11 @@
 interact('.post-it-interact').
   draggable({
-    onmove: dragMoveListener
+    onmove: dragMoveListener,
+    restrict: {
+      restriction: 'parent',
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 },
+      endOnly: true
+    }
   })
   .resizable({
     edges: { left: false, right: true, bottom: true, top: false }
